de834835d207145ac92be4bc3588681ae410c122
mozhgangd335 <156450058+mozhgangd335@users.noreply.github.com>
2024-01-16 11:33:06 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,42 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp38
+namespace ConsoleApp40
 {
-   
     class Program
     {
-        private static string name;
-
         static void Main(string[] args)
-
         {
-            List<Person> persons = new List<Person>();
-            while (true)
-            {
-                var option = GetInt("1.add person\n2.show persons");
-                switch (option)
-                {
-                    case 1:
-                        {
-                            var FirstName = GetString("enter your firstname");
-                            var LastName = GetString("enter your LastName");
-                            var person = new Person(FirstName, LastName);
-                            persons.Add(person);
-                            
-                            break;
-                        }
-                    case 2:
-                        {
-                            foreach (var item in persons)
-                            {
-                                item.GetFullName(item.FirstName, item.LastName);
-                            }
-                            break;
-                        }
-                }
-            } 
-    } public static string GetString(string message)
+            var Name = GetString("please enter your name:");
+            var Id = GetInt("enter your id:");
+            var Salary = GetDouble("enter your salary");
+            var department = GetString("enter your department:");
+            Manager manager = new Manager(Name, Id, Salary,department);
+            manager.PrintDetails();
+            Console.ReadKey();
+        }
+
+        
+
+        public static string GetString(string message)
         {
             Console.WriteLine(message);
             string value = Console.ReadLine();
@@ -51,4 +33,12 @@ namespace ConsoleApp38
             var value = int.Parse(Console.ReadLine());
             return value;
         }
-    } }
+        public static double GetDouble(string message)
+        {
+            Console.WriteLine(message);
+            var value = double.Parse(Console.ReadLine());
+            return value;
+        }
+
+    }
+}
